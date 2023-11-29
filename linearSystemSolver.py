5786cd3bb7bc860642f1f18f8a4743cb6bc66194
Voluptatemn <wqg1621@gmail.com>
2023-11-28 22:09:43 -0800

    commiut

--- linearSystemSolver.py
+++ linearSystemSolver.py
@@ -6,12 +6,18 @@ input uses numpy as such:
 
 import numpy as np
 
+def copy(array):
+    new_array = []
+    for i in range (len(array)):
+        new_array.append(array[i])
+    return new_array
+
 def replacement(matrix, row1, row2):
     matrix[row1] += matrix[row2]
     return matrix
 
 def interchange(matrix, row1, row2):
-    store = matrix[row1]
+    store = copy(matrix[row1])
     matrix[row1] = matrix[row2]
     matrix[row2] = store
     return matrix
@@ -20,11 +26,28 @@ def scaling(matrix, row, scale):
     matrix[row] *= scale
     return matrix
 
-matrix = np.array([[3, 3, 3, 3], [2, 2, 2, 4], [1, 1, 1, 5]])
-print(matrix.shape[1])
-print(scaling(matrix, 1, 3))
+matrix = np.array([[0, 3, 3, 3], [2, 2, 2, 4], [1, 1, 1, 5]])
+print(matrix)
+# print(interchange(matrix, 0, 1))
 
-def calculation(matrix):
+def calculation(matrix, index):
     row, column = matrix.shape
+    
+    # loop through the row to move the row with non zero Xo to row o
+    for i in range (index, row):
+        if matrix[i][index] != 0:
+            interchange(matrix, i, index)
+            break       
+    
+    # check if there is any row with a non-zero leading number
+    if matrix[index][index] == 0:
+        return calculation(matrix, index + 1)
+    
+    
+    
+    
+    return 
+
+calculation(matrix, 0)
     
         
