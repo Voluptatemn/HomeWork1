8cb2b2e62b239f936d9e2a991ab0e8ec8df02445
Voluptatemn <wqg1621@gmail.com>
2023-11-29 22:22:43 -0800

    commit

--- linearSystemSolver.py
+++ linearSystemSolver.py
@@ -68,8 +68,6 @@ def calculation(matrix, index):
     # move the iteration into the next step
     return calculation(matrix, index + 1)
 
-
-
 def allzero(row, consistency = 1):
     check = True
     for i in range (len(row) - consistency):
